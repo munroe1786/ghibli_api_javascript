dcc6d2cd9a0d3affc9a387cfe484836f399b3a83
munroe1786 <era3munroe@gmail.com>
2021-04-20 05:52:11 -0500

    add XMLHttpRequest to access API for now

--- scripts.js
+++ scripts.js
@@ -1,12 +1,30 @@
 // Replace ./data.json with your JSON feed
-fetch('https://ghibliapi.herokuapp.com/films')
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
+//fetch('https://ghibliapi.herokuapp.com/films')
+  //.then((response) => {
+  //  return response.json()
+  //})
+  //.then((data) => {
     // Work with JSON data here
-    console.log(data)
-  })
-  .catch((err) => {
+    //console.log(data)
+  //})
+  //.catch((err) => {
     // Do something for an error here
-  })
+  //})
+
+  var request = new XMLHttpRequest()
+
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach((movie) => {
+      console.log(movie.title)
+    })
+  } else {
+    console.log('error')
+  }
+}
+
+request.send()
