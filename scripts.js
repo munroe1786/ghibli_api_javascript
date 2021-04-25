5450d38cd7c4df7bfc2d088a5aad48a56094152b
munroe1786 <era3munroe@gmail.com>
2021-04-25 18:56:39 -0500

    reorganize code to follow tutorial

--- scripts.js
+++ scripts.js
@@ -11,31 +11,32 @@
     // Do something for an error here
   //})
 
-//var request = new XMLHttpRequest()
+  const app = document.getElementById('root')
 
-//request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-//request.onload = function () {
+  const logo = document.createElement('img')
+  logo.src = 'logo.png'
+  
+  const container = document.createElement('div')
+  container.setAttribute('class', 'container')
+  
+  app.appendChild(logo)
+  app.appendChild(container)
+
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.onload = function () {
   // Begin accessing JSON data here
-  //var data = JSON.parse(this.response)
+  var data = JSON.parse(this.response)
 
-  //if (request.status >= 200 && request.status < 400) {
-    //data.forEach((movie) => {
-      //console.log(movie.title)
-    //})
-  //} else {
-    //console.log('error')
-  //}
-//}
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach((movie) => {
+      console.log(movie.title)
+      console.log(movie.description)
+    })
+  } else {
+    console.log('error')
+  }
+}
 
-//request.send()
-
-const app = document.getElementById('root')
-
-const logo = document.createElement('img')
-logo.src = 'logo.png'
-
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
-
-app.appendChild(logo)
-app.appendChild(container)
+request.send()
