d26db4998f051bce52a6ce932611ea3df172e51e
munroe1786 <era3munroe@gmail.com>
2021-04-24 19:07:48 -0500

    add logo.src / add container const

--- scripts.js
+++ scripts.js
@@ -32,3 +32,7 @@
 const app = document.getElementById('root')
 
 const logo = document.createElement('img')
+logo.src = 'logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
