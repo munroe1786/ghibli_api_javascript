7abeab65688ab8a73ed20b7d712b9ad178795871
munroe1786 <era3munroe@gmail.com>
2021-05-01 21:37:49 -0500

    build out card elements

--- scripts.js
+++ scripts.js
@@ -11,16 +11,16 @@
     // Do something for an error here
   //})
 
-  const app = document.getElementById('root')
+const app = document.getElementById('root')
 
-  const logo = document.createElement('img')
-  logo.src = 'logo.png'
+const logo = document.createElement('img')
+logo.src = 'logo.png'
   
-  const container = document.createElement('div')
-  container.setAttribute('class', 'container')
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
   
-  app.appendChild(logo)
-  app.appendChild(container)
+app.appendChild(logo)
+app.appendChild(container)
 
 var request = new XMLHttpRequest()
 
@@ -31,8 +31,19 @@ request.onload = function () {
 
   if (request.status >= 200 && request.status < 400) {
     data.forEach((movie) => {
-      console.log(movie.title)
-      console.log(movie.description)
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.title
+
+      const p = document.createElement('p')
+      movie.description = movie.description.substring(0, 300)
+      p.textContent = `${movie.description}...`
+
+      container.appendChild(card)
+      card.appendChild(h1)
+      card.appendChild(p)
     })
   } else {
     console.log('error')
