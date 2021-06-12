45cd43cc8138111c9c4718ed45ca299535739489
munroe1786 <era3munroe@gmail.com>
2021-06-11 22:50:00 -0500

    add ghibli logo image as logo.png and change logo.src in scripts.js to logo.png

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@
 const app = document.getElementById('root');
 
 const logo = document.createElement('img');
-logo.src = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2Fc%2Fca%2FStudio_Ghibli_logo.svg%2F1200px-Studio_Ghibli_logo.svg.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FStudio_Ghibli&tbnid=U7MOowdpqYHy-M&vet=12ahUKEwixmsGYvazwAhWPhK0KHZVwDPIQMygAegUIARC9AQ..i&docid=YGHRdLByY8ZjoM&w=1200&h=576&q=studio%20ghibli%20logo&ved=2ahUKEwixmsGYvazwAhWPhK0KHZVwDPIQMygAegUIARC9AQ"
+logo.src = 'logo.png';
   
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
@@ -31,25 +31,25 @@ request.onload = function () {
 
   if (request.status >= 200 && request.status < 400) {
     data.forEach((movie) => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
 
-      const h1 = document.createElement('h1')
-      h1.textContent = movie.title
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
 
-      const p = document.createElement('p')
+      const p = document.createElement('p');
       movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
+      p.textContent = `${movie.description}...`;
 
-      container.appendChild(card)
-      card.appendChild(h1)
-      card.appendChild(p)
-    })
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
   } else {
-    const errorMessage = document.createElement('error')
-    errorMessage.textContent = `Oh no, something went wrong!`
-    app.appendChild(errorMessage)
+    const errorMessage = document.createElement('error');
+    errorMessage.textContent = `Oh no, something went wrong!`;
+    app.appendChild(errorMessage);
   }
 }
 
-request.send()
+request.send();
